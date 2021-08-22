cfef410e5a3a4e41ef7e991a4993f46475ee4931
mohamedkhidr <43112812+mohamedkhidr@users.noreply.github.com>
2021-08-22 12:26:39 +0200

    Update root.dart

--- lib/root.dart
+++ lib/root.dart
@@ -4,15 +4,15 @@ class Root {
   static const MethodChannel _channel = const MethodChannel('root');
 
 // Function to Run shell commands
-  static Future<String?> exec({String? cmd}) async {
-    String? result =
+  static Future<String> exec({String cmd}) async {
+    String result =
         await _channel.invokeMethod('ExecuteCommand', {"cmd": cmd});
     return result;
   }
 
 // Function to check Root status
-  static Future<bool?> isRooted() async {
-    bool? result = await _channel.invokeMethod("isRooted");
+  static Future<bool> isRooted() async {
+    bool result = await _channel.invokeMethod("isRooted");
     return result;
   }
 }
